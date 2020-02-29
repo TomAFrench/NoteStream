26bf5ef955e4fc708553cf464f33e422ab6dc065
Moe Adham <moe@bitaccess.co>
2020-02-29 22:03:30 +0000

    [Proofs] some pseudocode that is destined to fail

--- client/src/App.js
+++ client/src/App.js
@@ -1,132 +1,132 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { getWeb3 } from "./utils";
-import "./styles.css";
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { getWeb3 } from './utils'
+import './styles.css'
 import {
   days as daysOption,
   hours as hoursOption,
   minutes as minutesOption
-} from "./options";
-const { JoinSplitProof, DividendProof, note } = require("aztec.js");
-const secp256k1 = require("@aztec/secp256k1");
-const zkAssetAddress = "0x54Fac13e652702a733464bbcB0Fb403F1c057E1b";
-const streamContractAddress = "0x1f52693c618d093cEF45Bc59100C9086B3108a61";
-const payeeAddress = "0xC6EBff8Bdb7a8E05A350676f8b662231e87D83a7";
+} from './options'
+const { JoinSplitProof, DividendProof, note } = require('aztec.js')
+const secp256k1 = require('@aztec/secp256k1')
+const zkAssetAddress = '0x54Fac13e652702a733464bbcB0Fb403F1c057E1b'
+const streamContractAddress = '0x1f52693c618d093cEF45Bc59100C9086B3108a61'
+const payeeAddress = '0xC6EBff8Bdb7a8E05A350676f8b662231e87D83a7'
 
-const streamContract = require("./streamContract.js");
+const streamContract = require('./streamContract.js')
 
 const App = () => {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [network, setNetwork] = useState(undefined);
-  const [streamAmount, setStreamAmount] = useState(null);
-  const [depositAmount, setDepositAmount] = useState(null);
-  const [destinationAddress, setDestinationAddress] = useState(null);
-  const [duration, setDuration] = useState(null);
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [route, setRoute] = useState("deposit");
-  const [daiBalance, setDaiBalance] = useState(0);
-  const [zkdaiBalance, setZkdaiBalance] = useState(0);
-  const [notes, setNotes] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-  const [asset, setAsset] = useState(null);
+  const [web3, setWeb3] = useState(null)
+  const [accounts, setAccounts] = useState(null)
+  const [contract, setContract] = useState(null)
+  const [network, setNetwork] = useState(undefined)
+  const [streamAmount, setStreamAmount] = useState(null)
+  const [depositAmount, setDepositAmount] = useState(null)
+  const [destinationAddress, setDestinationAddress] = useState(null)
+  const [duration, setDuration] = useState(null)
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [route, setRoute] = useState('deposit')
+  const [daiBalance, setDaiBalance] = useState(0)
+  const [zkdaiBalance, setZkdaiBalance] = useState(0)
+  const [notes, setNotes] = useState(null)
+  const [loaded, setLoaded] = useState(false)
+  const [asset, setAsset] = useState(null)
 
-  async function depositZKtoken(depositAmount) {
-    console.log("lefut", depositAmount);
+  async function depositZKtoken (depositAmount) {
+    console.log('lefut', depositAmount)
     await asset.deposit(
-      [
-        {
-          to: accounts[0],
-          amount: depositAmount
-        }
-      ],
-      {}
-    );
+      [{ to: accounts[0], amount: depositAmount }], {})
     setTimeout(() => {
-      getBalance(asset);
-    }, 5000);
+      getBalance(asset)
+    }, 15000)
   }
-  async function getBalance(asset) {
-    const _zkbalance = await asset.balance();
-    setZkdaiBalance(_zkbalance);
+  async function getBalance (asset) {
+    const _zkbalance = await asset.balance()
+    setZkdaiBalance(_zkbalance)
   }
 
-  async function getAllNotes(asset) {
+  async function getAllNotes (asset) {
     // Fetch all notes
-    const _notes = await asset.fetchNotesFromBalance();
-    setNotes(_notes);
+    const _notes = await asset.fetchNotesFromBalance()
+    setNotes(_notes)
   }
 
-  async function generateDividendProof(zA, zB) {
+  async function generateDividendProof (originalNote, zA, zB) {
     /// / //// PROOF STUFF here.
-    const acts = [secp256k1.generateAccount(), secp256k1.generateAccount()];
-    const notionalNote = await note.create(acts[0].publicKey, 10); // Original Node
-    const targetNote = await note.create(acts[0].publicKey, 5); // Note to receiver
-    const residualNote = await note.create(
-      acts[1].publicKey,
-      1,
-      "0xf637cfb0c6be07eb0533d1600c7c3fe28df887a3"
-    );
+    const acts = [secp256k1.generateAccount(), secp256k1.generateAccount()]
+
+    const notionalNote = originalNote// await note.create(acts[0].publicKey, 10) // Original Node
+    const targetNote = await note.create(acts[0].publicKey, 5) // Note to receiver
+    const residualNote = await note.create(acts[1].publicKey, 1, streamContractAddress)
 
     // notionalNote.value * za = targetNote.value * zb + residualNote.value
     // 10 * 1 = 5 * 2 + 1
 
-    const sender = acts[0].address; // address of transaction sender
-    const publicOwner = acts[0].address; // address of public token owner
+    const sender = acts[0].address // address of transaction sender
+    const publicOwner = acts[0].address // address of public token owner
 
-    const proof = new DividendProof(
-      notionalNote,
-      residualNote,
-      targetNote,
-      sender,
-      zA,
-      zB
-    );
-    console.log(proof);
-  }
+    const dividendProof = new DividendProof(notionalNote, residualNote, targetNote, sender, zA, zB)
+    const dividendProofABI = dividendProof.encodeABI()
 
-  async function createStream() {
-    const noteForStreamContract = await depositToStream(streamAmount);
+    const inputNotes = []
+    const outputNotes = []
+    const publicValue = -10 // input notes contain 10 fewer than output notes = deposit of 10 public tokens
+    const JSsender = accounts[0].address // address of transaction sender
+    const JSproof = new JoinSplitProof(inputNotes, outputNotes, JSsender, publicValue, publicOwner)
+    const JSproofABI = JSproof.encodeABI()
+    const durationToWithdraw = 1
+
+    const streamId = 3
     const streamContractInstance = new web3.eth.Contract(
       streamContract.abi,
       streamContractAddress
-    );
-    console.log(streamContractInstance.methods);
-    let startTime = Math.floor(Date.now() / 1000);
-    console.log("days", days, "hours", hours, "minutes", minutes);
-    let endTime = startTime + days * 86400 + hours * 3600 + minutes * 60;
-    console.log(
+    )
+    streamContractInstance.methods.withdrawFromStream(
+      streamId,
+      dividendProofABI,
+      JSproofABI,
+      durationToWithdraw
+    ).send({ from: accounts[0] }, (err, streamID) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Steam ID created', streamID)
+      }
+    })
+  }
+
+  async function createStream () {
+    const noteForStreamContract = await depositToStream(streamAmount)
+    const streamContractInstance = new web3.eth.Contract(
+      streamContract.abi,
+      streamContractAddress
+    )
+    console.log(streamContractInstance.methods)
+    const startTime = Math.floor(Date.now() / 1000)
+    console.log('days', days, 'hours', hours, 'minutes', minutes)
+    const endTime = startTime + days * 86400 + hours * 3600 + minutes * 60
+    console.log(destinationAddress, noteForStreamContract.noteHash, zkAssetAddress, startTime, startTime + 1000)
+    console.log('account', accounts[0])
+    streamContractInstance.methods.createStream(
       destinationAddress,
       noteForStreamContract.noteHash,
       zkAssetAddress,
-      startTime,
+      startTime + 30,
       startTime + 1000
-    );
-
-    console.log("account", accounts[0]);
-    
-    streamContractInstance.methods
-      .createStream(
-        destinationAddress,
-        noteForStreamContract.noteHash,
-        zkAssetAddress,
-        startTime + 30000,
-        startTime + 10000000
-      )
-      .send({ from: accounts[0] }, (err, streamID) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Steam ID", streamID);
-        }
-      });
+    ).send({ from: accounts[0] }, (err, streamID) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Steam ID created', streamID)
+      }
+    })
+    await generateDividendProof(noteForStreamContract, 1, 1)
   }
 
-  async function depositToStream(sendAmount) {
-    console.log("_asset", asset);
+  async function depositToStream (sendAmount) {
+    console.log('_asset', asset)
     const _sendResp = await asset.send(
       [
         {
@@ -137,83 +137,83 @@ const App = () => {
         }
       ],
       { userAccess: [payeeAddress] }
-    ); // account of user who is streaming
-    console.info("sent funds confidentially");
-    console.log("_sendResp", _sendResp);
-    let noteForStreamContract = null;
-    _sendResp.outputNotes.forEach(function(outputNote) {
+    ) // account of user who is streaming
+    console.info('sent funds confidentially')
+    console.log('_sendResp', _sendResp)
+    let noteForStreamContract = null
+    _sendResp.outputNotes.forEach(function (outputNote) {
       if (outputNote.owner === streamContractAddress) {
-        noteForStreamContract = outputNote;
+        noteForStreamContract = outputNote
       }
-    });
-    console.log("noteForStreamContract", noteForStreamContract);
-    return noteForStreamContract;
+    })
+    console.log('noteForStreamContract', noteForStreamContract)
+    return noteForStreamContract
   }
 
   useEffect(() => {
     const init = async () => {
-      const _web3 = await getWeb3();
-      const _accounts = await _web3.eth.getAccounts();
-      const _network = await _web3.eth.net.getId();
-      setNetwork(_network);
+      const _web3 = await getWeb3()
+      const _accounts = await _web3.eth.getAccounts()
+      const _network = await _web3.eth.net.getId()
+      setNetwork(_network)
 
       /* const _contract = new web3.eth.Contract(
        Multisig.abi,
         deployedNetwork && deployedNetwork.address,
       ); */
-      console.log("web3", _web3);
-      console.log("windowaztec", window.aztec);
-      setWeb3(_web3);
-      setAccounts(_accounts);
-      console.log("accounts", _accounts);
+      console.log('web3', _web3)
+      console.log('windowaztec', window.aztec)
+      setWeb3(_web3)
+      setAccounts(_accounts)
+      console.log('accounts', _accounts)
       // setContract(_contract);
-      const apiKey = "test1234";
-      const result = await window.aztec.enable({ apiKey });
+      const apiKey = 'test1234'
+      const result = await window.aztec.enable({ apiKey })
       // Fetch the zkAsset
 
-      const _asset = await window.aztec.zkAsset(zkAssetAddress);
-      setAsset(_asset);
-      console.log("ASSET:", _asset);
-      await getBalance(_asset);
-      await getAllNotes(_asset);
-      setLoaded(true);
+      const _asset = await window.aztec.zkAsset(zkAssetAddress)
+      setAsset(_asset)
+      console.log('ASSET:', _asset)
+      await getBalance(_asset)
+      await getAllNotes(_asset)
+      setLoaded(true)
       // Send funds
-      const deposit = true;
+      const deposit = true
       if (false) {
         //  createStream(_web3, noteForStreamContract, 0, 10000000);
       }
-    };
-    init();
+    }
+    init()
     /*
     window.ethereum.on("accountsChanged", accounts => {
       setAccounts(accounts);
     });
     */
-  }, []);
+  }, [])
 
   useEffect(() => {
-    console.log("minutes", minutes);
-  }, [minutes]);
+    console.log('minutes', minutes)
+  }, [minutes])
 
   return (
-    <div className="App">
-      <div style={{ width: 500, margin: "auto", marginTop: 150 }}>
+    <div className='App'>
+      <div style={{ width: 500, margin: 'auto', marginTop: 150 }}>
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            backgroundColor: "rgba(8, 8, 8, 0.44)",
-            alignItems: "center",
+            display: 'flex',
+            justifyContent: 'space-between',
+            backgroundColor: 'rgba(8, 8, 8, 0.44)',
+            alignItems: 'center',
             marginBottom: 20
           }}
         >
           <p
-            onClick={() => loaded && setRoute("deposit")}
+            onClick={() => loaded && setRoute('deposit')}
             style={{
-              cursor: "pointer",
+              cursor: 'pointer',
               margin: 0,
-              padding: "5px 30px 5px 30px",
-              backgroundColor: route == "deposit" ? "#0179C4" : null,
+              padding: '5px 30px 5px 30px',
+              backgroundColor: route == 'deposit' ? '#0179C4' : null,
               borderRadius: 10,
               opacity: 0.85
             }}
@@ -221,12 +221,12 @@ const App = () => {
             Deposit
           </p>
           <p
-            onClick={() => setRoute("create")}
+            onClick={() => setRoute('create')}
             style={{
-              cursor: "pointer",
+              cursor: 'pointer',
               margin: 0,
-              padding: "5px 30px 5px 30px",
-              backgroundColor: route == "create" ? "#0179C4" : null,
+              padding: '5px 30px 5px 30px',
+              backgroundColor: route == 'create' ? '#0179C4' : null,
               borderRadius: 10,
               opacity: 0.85
             }}
@@ -234,12 +234,12 @@ const App = () => {
             Create
           </p>
           <p
-            onClick={() => setRoute("status")}
+            onClick={() => setRoute('status')}
             style={{
-              cursor: "pointer",
+              cursor: 'pointer',
               margin: 0,
-              padding: "5px 30px 5px 30px",
-              backgroundColor: route == "status" ? "#0179C4" : null,
+              padding: '5px 30px 5px 30px',
+              backgroundColor: route == 'status' ? '#0179C4' : null,
               borderRadius: 10,
               opacity: 0.85
             }}
@@ -248,24 +248,24 @@ const App = () => {
           </p>
         </div>
 
-        {route == "create" ? (
+        {route == 'create' ? (
           <>
-            <div className="input-wrap">
+            <div className='input-wrap'>
               <label>How much do you want to stream?</label>
               <input
-                type="text"
+                type='text'
                 onChange={val => setStreamAmount(val.target.value)}
                 value={streamAmount}
-                placeholder="0 Dai"
+                placeholder='0 Dai'
               />
             </div>
-            <div className="input-wrap">
+            <div className='input-wrap'>
               <label>What is the destination address?</label>
               <input
-                type="text"
+                type='text'
                 onChange={val => setDestinationAddress(val.target.value)}
                 value={destinationAddress}
-                placeholder="0xe065D88f41615231e69026040C075d9F9F1bD00A"
+                placeholder='0xe065D88f41615231e69026040C075d9F9F1bD00A'
               />
             </div>
             <p style={{ marginBottom: 10 }}>
@@ -273,12 +273,12 @@ const App = () => {
             </p>
             <div
               style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignSelf: "center"
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignSelf: 'center'
               }}
             >
-              <div className="input-wrap select-wrap">
+              <div className='input-wrap select-wrap'>
                 <label>Days</label>
                 <select
                   value={days}
@@ -292,7 +292,7 @@ const App = () => {
                 </select>
               </div>
 
-              <div className="input-wrap select-wrap">
+              <div className='input-wrap select-wrap'>
                 <label>Hours</label>
                 <select
                   value={hours}
@@ -305,7 +305,7 @@ const App = () => {
                   ))}
                 </select>
               </div>
-              <div className="input-wrap select-wrap">
+              <div className='input-wrap select-wrap'>
                 <label>Minutes</label>
                 <select
                   value={minutes}
@@ -321,10 +321,10 @@ const App = () => {
             </div>
 
             <div
-              className="backbutton"
+              className='backbutton'
               style={{
-                display: "flex",
-                justifyContent: "center",
+                display: 'flex',
+                justifyContent: 'center',
                 marginTop: 20
               }}
             >
@@ -334,27 +334,27 @@ const App = () => {
             </div>
           </>
         ) : null}
-        {route == "deposit" ? (
+        {route == 'deposit' ? (
           <>
             <p>Your Dai Balance: {daiBalance} Dai</p>
             <p style={{ marginBottom: 20 }}>
               Your zkDai Balance: {zkdaiBalance} ZkDai
             </p>
-            <div className="input-wrap">
+            <div className='input-wrap'>
               <label>Enter deposit/Withdraw amount</label>
               <input
-                type="text"
+                type='text'
                 onChange={val => setDepositAmount(val.target.value)}
                 value={depositAmount}
-                placeholder="0 Dai/zkDai"
+                placeholder='0 Dai/zkDai'
               />
             </div>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <div
-                className="backbutton"
+                className='backbutton'
                 style={{
-                  display: "flex",
-                  justifyContent: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
                   marginTop: 20
                 }}
               >
@@ -366,10 +366,10 @@ const App = () => {
                 </button>
               </div>
               <div
-                className="backbutton"
+                className='backbutton'
                 style={{
-                  display: "flex",
-                  justifyContent: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
                   marginTop: 20
                 }}
               >
@@ -382,7 +382,7 @@ const App = () => {
         ) : null}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
