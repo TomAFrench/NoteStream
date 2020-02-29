e5ba6708f8ebc6fb4814240da01f42617ae21dee
Gyuri <gyorgykloczl@gmail.com>
2020-02-29 19:59:04 +0000

    deposit screen

--- client/src/App.js
+++ client/src/App.js
@@ -14,13 +14,16 @@ function App() {
   const [accounts, setAccounts] = useState(null);
   const [contract, setContract] = useState(null);
   const [network, setNetwork] = useState(undefined);
-  const [amount, setAmount] = useState(null);
+  const [streamAmount, setStreamAmount] = useState(null);
+  const [depositAmount, setDepositAmount] = useState(null);
   const [recipient, setRecipient] = useState(null);
   const [duration, setDuration] = useState(null);
   const [days, setDays] = useState(null);
   const [hours, setHours] = useState(null);
   const [minutes, setMinutes] = useState(null);
   const [route, setRoute] = useState("deposit");
+  const [daiBalance, setDaiBalance] = useState(0);
+  const [zkdaiBalance, setZkdaiBalance] = useState(0);
 
   useEffect(() => {
     const init = async () => {
@@ -61,7 +64,7 @@ function App() {
             justifyContent: "space-between",
             backgroundColor: "rgba(8, 8, 8, 0.44)",
             alignItems: "center",
-            marginBottom: 10
+            marginBottom: 20
           }}
         >
           <p
@@ -78,17 +81,17 @@ function App() {
             Deposit
           </p>
           <p
-            onClick={() => setRoute("send")}
+            onClick={() => setRoute("create")}
             style={{
               cursor: "pointer",
               margin: 0,
               padding: "5px 30px 5px 30px",
-              backgroundColor: route == "send" ? "#0179C4" : null,
+              backgroundColor: route == "create" ? "#0179C4" : null,
               borderRadius: 10,
               opacity: 0.85
             }}
           >
-            Send
+            Create
           </p>
           <p
             onClick={() => setRoute("status")}
@@ -104,74 +107,136 @@ function App() {
             Status
           </p>
         </div>
-        <div className="input-wrap">
-          <label>How much do you want to stream?</label>
-          <input
-            type="text"
-            onChange={val => setAmount(val.target.value)}
-            value={amount}
-            placeholder="0 Dai"
-          />
-        </div>
-        <div className="input-wrap">
-          <label>Who is the recipient?</label>
-          <input
-            type="text"
-            onChange={val => setRecipient(val.target.value)}
-            value={recipient}
-            placeholder="0xe065D88f41615231e69026040C075d9F9F1bD00A"
-          />
-        </div>
-        <p style={{ marginBottom: 10 }}>For how long do you want to stream?</p>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignSelf: "center"
-          }}
-        >
-          <div className="input-wrap select-wrap">
-            <label>Days</label>
-            <select value={days} onChange={val => setDays(val.target.value)}>
-              {daysOption.map(option => (
-                <option key={option.id} value={option.title}>
-                  {option.title}
-                </option>
-              ))}
-            </select>
-          </div>
 
-          <div className="input-wrap select-wrap">
-            <label>Hours</label>
-            <select value={hours} onChange={val => setHours(val.target.value)}>
-              {hoursOption.map(option => (
-                <option key={option.id} value={option.title}>
-                  {option.title}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="input-wrap select-wrap">
-            <label>Minutes</label>
-            <select
-              value={minutes}
-              onChange={val => setMinutes(val.target.value)}
+        {route == "create" ? (
+          <>
+            <div className="input-wrap">
+              <label>How much do you want to stream?</label>
+              <input
+                type="text"
+                onChange={val => setStreamAmount(val.target.value)}
+                value={streamAmount}
+                placeholder="0 Dai"
+              />
+            </div>
+            <div className="input-wrap">
+              <label>Who is the recipient?</label>
+              <input
+                type="text"
+                onChange={val => setRecipient(val.target.value)}
+                value={recipient}
+                placeholder="0xe065D88f41615231e69026040C075d9F9F1bD00A"
+              />
+            </div>
+            <p style={{ marginBottom: 10 }}>
+              For how long do you want to stream?
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignSelf: "center"
+              }}
             >
-              {minutesOption.map(option => (
-                <option key={option.id} value={option.title}>
-                  {option.title}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
+              <div className="input-wrap select-wrap">
+                <label>Days</label>
+                <select
+                  value={days}
+                  onChange={val => setDays(val.target.value)}
+                >
+                  {daysOption.map(option => (
+                    <option key={option.id} value={option.title}>
+                      {option.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
 
-        <div
-          className="backbutton"
-          style={{ display: "flex", justifyContent: "center", marginTop: 20 }}
-        >
-          <button onClick={() => window.close()}>Send</button>
-        </div>
+              <div className="input-wrap select-wrap">
+                <label>Hours</label>
+                <select
+                  value={hours}
+                  onChange={val => setHours(val.target.value)}
+                >
+                  {hoursOption.map(option => (
+                    <option key={option.id} value={option.title}>
+                      {option.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="input-wrap select-wrap">
+                <label>Minutes</label>
+                <select
+                  value={minutes}
+                  onChange={val => setMinutes(val.target.value)}
+                >
+                  {minutesOption.map(option => (
+                    <option key={option.id} value={option.title}>
+                      {option.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div
+              className="backbutton"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: 20
+              }}
+            >
+              <button onClick={() => window.close()}>Create stream</button>
+            </div>
+          </>
+        ) : null}
+        {route == "deposit" ? (
+          <>
+            <p>
+              Your Dai Balance: {daiBalance} Dai
+            </p>
+            <p style={{ marginBottom: 20 }}>
+              Your zkDai Balance: {zkdaiBalance} ZkDai
+            </p>
+            <div className="input-wrap">
+              <label>Enter deposit/Withdraw amount</label>
+              <input
+                type="text"
+                onChange={val => setDepositAmount(val.target.value)}
+                value={depositAmount}
+                placeholder="0 Dai/zkDai"
+              />
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div
+                className="backbutton"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: 20
+                }}
+              >
+                <button style={{ width: 200 }} onClick={() => window.close()}>
+                  Deposit
+                </button>
+              </div>
+              <div
+                className="backbutton"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: 20
+                }}
+              >
+                <button style={{ width: 200 }} onClick={() => window.close()}>
+                  Withdraw
+                </button>
+              </div>
+            </div>
+          </>
+        ) : null}
       </div>
     </div>
   );
