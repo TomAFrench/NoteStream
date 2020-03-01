e888d729a7f7bc8953a935daa37de088d8c8ee89
Gyuri <gyorgykloczl@gmail.com>
2020-03-01 03:35:29 +0000

    create date fix

--- client/src/views/create.js
+++ client/src/views/create.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
-import '../styles.css'
+import React, { useState } from "react";
+import "../styles.css";
 import {
   days as daysOption,
   hours as hoursOption,
   minutes as minutesOption
-} from '../options'
+} from "../options";
 
-const Create = ({web3, zkAsset, streamContract}) => {
-  const [streamAmount, setStreamAmount] = useState(null)
-  const [recipient, setRecipient] = useState(null)
-  const [days, setDays] = useState(null)
-  const [hours, setHours] = useState(null)
-  const [minutes, setMinutes] = useState(null)
+const Create = ({ web3, zkAsset, streamContract }) => {
+  const [streamAmount, setStreamAmount] = useState(null);
+  const [recipient, setRecipient] = useState(null);
+  const [days, setDays] = useState(null);
+  const [hours, setHours] = useState(null);
+  const [minutes, setMinutes] = useState(null);
 
-  function initialiseStream (
+  function initialiseStream(
     streamContract,
     payeeAddress,
     noteForStreamContract,
     startTime,
     endTime
   ) {
-    console.log(streamContract.methods)
+    console.log(streamContract.methods);
     return streamContract.methods
       .createStream(
         recipient,
@@ -31,15 +31,20 @@ const Create = ({web3, zkAsset, streamContract}) => {
       )
       .call({}, (err, streamID) => {
         if (err) {
-          console.log(err)
+          console.log(err);
         } else {
-          console.log('Steam ID', streamID)
-          return streamID
+          console.log("Steam ID", streamID);
+          return streamID;
         }
-      })
+      });
   }
 
-  async function fundStream (streamContractAddress, payeeAddress, sendAmount, asset) {
+  async function fundStream(
+    streamContractAddress,
+    payeeAddress,
+    sendAmount,
+    asset
+  ) {
     const _sendResp = await asset.send(
       [
         {
@@ -50,112 +55,132 @@ const Create = ({web3, zkAsset, streamContract}) => {
         }
       ],
       { userAccess: [payeeAddress] }
-    ) // account of user who is streaming
-    console.info('sent funds confidentially')
-    console.log('_sendResp', _sendResp)
-    let noteForStreamContract = null
-    _sendResp.outputNotes.forEach(function (outputNote) {
+    ); // account of user who is streaming
+    console.info("sent funds confidentially");
+    console.log("_sendResp", _sendResp);
+    let noteForStreamContract = null;
+    _sendResp.outputNotes.forEach(function(outputNote) {
       if (outputNote.owner === streamContractAddress) {
-        noteForStreamContract = outputNote
+        noteForStreamContract = outputNote;
       }
-    })
-    console.log('noteForStreamContract', noteForStreamContract)
-    return noteForStreamContract
+    });
+    console.log("noteForStreamContract", noteForStreamContract);
+    return noteForStreamContract;
   }
 
-  async function createStream(
-    sendAmount,
-    payeeAddress,
-    startTime,
-    endTime ) {
-      const streamNote = await fundStream(streamContract.address, payeeAddress, sendAmount, zkAsset)
-      return initialiseStream (streamContract, payeeAddress, streamNote, startTime, endTime)
+  async function createStream(sendAmount, payeeAddress, startTime, endTime) {
+    const streamNote = await fundStream(
+      streamContract.address,
+      payeeAddress,
+      sendAmount,
+      zkAsset
+    );
+    return initialiseStream(
+      streamContract,
+      payeeAddress,
+      streamNote,
+      startTime,
+      endTime
+    );
   }
 
   return (
     <>
-    <div className='input-wrap'>
-      <label>How much do you want to stream?</label>
-      <input
-        type='text'
-        onChange={val => setStreamAmount(val.target.value)}
-        value={streamAmount}
-        placeholder='0 Dai'
-      />
-    </div>
-    <div className='input-wrap'>
-      <label>Who is the recipient?</label>
-      <input
-        type='text'
-        onChange={val => setRecipient(val.target.value)}
-        value={recipient}
-        placeholder='0xe065D88f41615231e69026040C075d9F9F1bD00A'
-      />
-    </div>
-    <p style={{ marginBottom: 10 }}>
-      For how long do you want to stream?
-    </p>
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignSelf: 'center'
-      }}
-    >
-      <div className='input-wrap select-wrap'>
-        <label>Days</label>
-        <select
-          value={days}
-          onChange={val => setDays(val.target.value)}
-        >
-          {daysOption.map(option => (
-            <option key={option.id} value={option.title}>
-              {option.title}
-            </option>
-          ))}
-        </select>
+      <div className="input-wrap">
+        <label>How much do you want to stream?</label>
+        <input
+          type="text"
+          onChange={val => setStreamAmount(val.target.value)}
+          value={streamAmount}
+          placeholder="0 Dai"
+        />
+      </div>
+      <div className="input-wrap">
+        <label>Who is the recipient?</label>
+        <input
+          type="text"
+          onChange={val => setRecipient(val.target.value)}
+          value={recipient}
+          placeholder="0xe065D88f41615231e69026040C075d9F9F1bD00A"
+        />
+      </div>
+      <p style={{ marginBottom: 10 }}>For how long do you want to stream?</p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignSelf: "center"
+        }}
+      >
+        <div className="input-wrap select-wrap">
+          <label>Days</label>
+          <select
+            value={days}
+            onChange={val => {
+              console.log("val", val.target.value.split(" ")[0]);
+              setDays(val.target.value);
+            }}
+          >
+            {daysOption.map(option => (
+              <option key={option.id} value={option.title}>
+                {option.title}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="input-wrap select-wrap">
+          <label>Hours</label>
+          <select value={hours} onChange={val => setHours(val.target.value)}>
+            {hoursOption.map(option => (
+              <option key={option.id} value={option.title}>
+                {option.title}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="input-wrap select-wrap">
+          <label>Minutes</label>
+          <select
+            value={minutes}
+            onChange={val => setMinutes(val.target.value)}
+          >
+            {minutesOption.map(option => (
+              <option key={option.id} value={option.title}>
+                {option.title}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
 
-      <div className='input-wrap select-wrap'>
-        <label>Hours</label>
-        <select
-          value={hours}
-          onChange={val => setHours(val.target.value)}
+      <div
+        className="backbutton"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 20
+        }}
+      >
+        <button
+          onClick={() =>
+            createStream(
+              web3,
+              streamAmount,
+              Date.now() + 20,
+              Date.now() +
+                20 +
+                days.split(" ")[0] * 86400 +
+                hours.split(" ")[0] * 3600 +
+                minutes.split(" ")[0] * 60
+            )
+          }
         >
-          {hoursOption.map(option => (
-            <option key={option.id} value={option.title}>
-              {option.title}
-            </option>
-          ))}
-        </select>
+          Create stream
+        </button>
       </div>
-      <div className='input-wrap select-wrap'>
-        <label>Minutes</label>
-        <select
-          value={minutes}
-          onChange={val => setMinutes(val.target.value)}
-        >
-          {minutesOption.map(option => (
-            <option key={option.id} value={option.title}>
-              {option.title}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-
-    <div
-      className='backbutton'
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: 20
-      }}
-    >
-      <button onClick={() => createStream(web3, streamAmount, Date.now()+20, Date.now()+200)}>Create stream</button>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
