98c5377f55efc76dcdf98cfa21329293a286f90e
Tom French <tom@tomfren.ch>
2020-03-23 09:28:09 +0000

    chore(contracts): removed migrations

--- packages/contracts/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-/* global artifacts */
-const Migrations = artifacts.require('./Migrations.sol');
-
-module.exports = async (deployer) => {
-  const migrations = await Migrations.new();
-  Migrations.setAsDeployed(migrations);
-};
