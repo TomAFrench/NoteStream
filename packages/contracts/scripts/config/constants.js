4cdedbed86403a3fdf955284f92f0fc2d58848d1
Tom French <tom@tomfren.ch>
2020-05-14 10:07:47 +0100

    refactor(contracts): remove unused aztec scripts

--- packages/contracts/scripts/config/constants.js
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_GSN_RELAYER_PORT = 8090;
-
-export default {
-  DEFAULT_GSN_RELAYER_PORT,
-};
--- packages/contracts/scripts/tasks/copy/index.js
+++
@@ -1,9 +0,0 @@
-import pipeTasks from "../../utils/pipeTasks";
-import copyContracts from "./copyContracts";
-
-export default async function copy({ onError, onClose } = {}) {
-  return pipeTasks([copyContracts], {
-    onError,
-    onClose,
-  });
-}
