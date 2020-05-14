842f0af00bf8d7829bf9748f5bd8d68c3e297b80
Tom French <tom@tomfren.ch>
2020-05-14 09:29:27 +0100

    chore(contracts): remove unused buidler task

--- packages/contracts/buidler.config.js
+++ packages/contracts/buidler.config.js
@@ -2,16 +2,6 @@ usePlugin("@nomiclabs/buidler-truffle5");
 usePlugin("@nomiclabs/buidler-etherscan");
 require("dotenv").config({ path: ".env.development" });
 
-// This is a sample Buidler task. To learn how to create your own go to
-// https://buidler.dev/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await web3.eth.getAccounts();
-
-  for (const account of accounts) {
-    console.log(account);
-  }
-});
-
 task("export", "Exports the contract ABIs", async () => {
   require("./scripts/export.js");
 });
