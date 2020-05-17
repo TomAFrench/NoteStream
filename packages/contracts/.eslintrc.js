9c268326edefe5a170809e9745e2fcdf4b4af5b9
Tom French <tom@tomfren.ch>
2020-05-17 14:20:39 +0100

    chore(contracts): remove no-console linting rule from scripts folder

--- packages/contracts/.eslintrc.js
+++ packages/contracts/.eslintrc.js
@@ -1,31 +1,37 @@
 module.exports = {
-  extends: [
-    "../../.eslintrc.js",
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  env: {
-    mocha: true,
-  },
-  rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        ts: "never",
-      },
+    extends: [
+        '../../.eslintrc.js',
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
-    "import/no-extraneous-dependencies": [
-      "error",
-      { devDependencies: ["**/*.js"] },
-    ],
-  },
-  overrides: [
-    {
-      files: ["test/**/*.js"],
-      rules: {
-        "func-names": "off",
-      },
+    env: {
+        mocha: true,
     },
-  ],
+    rules: {
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                ts: 'never',
+            },
+        ],
+        'import/no-extraneous-dependencies': [
+            'error',
+            { devDependencies: ['**/*.js'] },
+        ],
+    },
+    overrides: [
+        {
+            files: ['test/**/*.js'],
+            rules: {
+                'func-names': 'off',
+            },
+        },
+        {
+            files: ['scripts/**/*.js'],
+            rules: {
+                'no-console': 'off',
+            },
+        },
+    ],
 };
