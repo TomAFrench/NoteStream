e8a167b0c9ed803c1aa26ad54b2d152e384ae697
Tom French <tom@tomfren.ch>
2020-06-07 21:52:52 +0100

    refactor(react-app): refactor stream progress bars into own component

--- packages/react-app/src/components/display/StreamRow.tsx
+++ packages/react-app/src/components/display/StreamRow.tsx
@@ -1,22 +1,21 @@
-import React, { useState, useEffect, ReactElement, useMemo } from 'react';
+import React, { ReactElement } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@material-ui/core/Button';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 
 import moment from 'moment';
 
 import { Contract } from 'ethers';
-import calculateTime from '../../utils/time';
 
 import { withdrawFunds, cancelStream } from '../../utils/stream';
 import { Stream, ZkNote } from '../../types/types';
 
 import { useAztec } from '../../contexts/AztecContext';
 import { useAddress } from '../../contexts/OnboardContext';
-import { convertToTokenValueDisplay } from '../../utils/convertToTokenValue';
+import { convertToTokenValueDisplay } from '../../utils/units/convertToTokenValue';
+import DoubleProgressBar from './DoubleProgressBar';
 
 const StreamRow = ({
   stream,
@@ -40,7 +39,6 @@ const StreamRow = ({
     stopTime,
     zkAsset,
   } = stream;
-  const [timePercentage, setTimePercentage] = useState<number>(0);
 
   const displayValue =
     note.value &&
@@ -49,29 +47,6 @@ const StreamRow = ({
       zkAsset.scalingFactor,
       zkAsset.linkedToken.decimals,
     );
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const newTimePercentage = calculateTime(
-        moment.unix(startTime),
-        moment.unix(stopTime),
-      );
-      setTimePercentage(parseFloat(newTimePercentage.toFixed(2)));
-    }, 1000);
-    return (): void => {
-      clearInterval(intervalId);
-    };
-  }, [startTime, stopTime]);
-
-  const withdrawPercentage = useMemo(
-    () =>
-      calculateTime(
-        moment.unix(startTime),
-        moment.unix(stopTime),
-        moment.unix(lastWithdrawTime),
-      ),
-    [startTime, stopTime, lastWithdrawTime],
-  );
 
   const address = role === 'recipient' ? sender : recipient;
   const button =
@@ -95,13 +70,7 @@ const StreamRow = ({
       </Button>
     );
 
-  if (
-    !address ||
-    note.value === undefined ||
-    !zkAsset.symbol ||
-    !startTime ||
-    !stopTime
-  ) {
+  if (!address || note.value === undefined || !zkAsset.symbol) {
     return null;
   }
   return (
@@ -111,11 +80,10 @@ const StreamRow = ({
       </TableCell>
       <TableCell align="right">{`${displayValue} ${zkAsset.symbol}`}</TableCell>
       <TableCell align="right">
-        <LinearProgress variant="determinate" value={timePercentage} />
-        <LinearProgress
-          variant="determinate"
-          value={withdrawPercentage}
-          color="secondary"
+        <DoubleProgressBar
+          startTime={startTime}
+          stopTime={stopTime}
+          lastWithdrawTime={lastWithdrawTime}
         />
       </TableCell>
       <TableCell align="right">
