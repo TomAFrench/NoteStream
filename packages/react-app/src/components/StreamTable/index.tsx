73dfbcbd78fb6b8354038c161abb063747509312
Tom French <tom@tomfren.ch>
2020-06-08 16:50:23 +0100

    feat(react-app): update table have pagination + sorting

--- packages/react-app/src/components/StreamTable/index.tsx
+++ packages/react-app/src/components/StreamTable/index.tsx
@@ -1,42 +1,98 @@
-import React, { ReactElement } from 'react';
-import PropTypes from 'prop-types';
+import React, { ReactElement, useCallback } from 'react';
+import moment from 'moment';
 
-import { useQuery } from '@apollo/client';
-
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
+import { useQuery } from '@apollo/client';
 import { Contract } from 'ethers';
+import { Button, Grid, CircularProgress } from '@material-ui/core';
+import { generateColumns, STREAM_TABLE_ID, Column } from '../Table/columns';
+import { cellWidth } from '../Table/TableHead';
+import Table from '../Table';
 
 import {
   GET_SENDER_STREAMS,
   GET_RECIPIENT_STREAMS,
 } from '../../graphql/streams';
-
-import { Stream } from '../../types/types';
 import { useAddress } from '../../contexts/OnboardContext';
-import StreamRow from './StreamRow';
+import { Stream, Hash, ZkNote } from '../../types/types';
+import { cancelStream } from '../../utils/stream';
+import { convertToTokenValueDisplay } from '../../utils/units/convertToTokenValue';
 
-const StreamTable = ({
+import { useAztec } from '../../contexts/AztecContext';
+import useDecodedNote from '../../hooks/useDecodedNote';
+import useENSName from '../../hooks/useENSName';
+import DoubleProgressBar from '../display/DoubleProgressBar';
+
+type HumanReadableStream = {
+  id: number;
+  recipient: string;
+  sender: string;
+  noteHash: Hash;
+  humanDeposit: string;
+  humanStartTime: string;
+  humanStopTime: string;
+
+  zkAsset: any;
+};
+type TableRowData = HumanReadableStream & {
+  humanStartTimeOrder: number;
+  humanStopTimeOrder: number;
+  humanLastWithdrawTimeOrder: number;
+  cancelStream: Function;
+};
+
+const humanReadableStream = (stream: Stream): HumanReadableStream => {
+  const {
+    id,
+    recipient,
+    noteHash,
+    sender,
+    startTime,
+    stopTime,
+    zkAsset,
+  } = stream;
+  const humanStartTime: string = moment
+    .unix(startTime)
+    .format('MMM D, YYYY - HH:mm');
+  const humanStopTime: string = moment
+    .unix(stopTime)
+    .format('MMM D, YYYY - HH:mm');
+  const humanDeposit = '';
+
+  return {
+    id,
+    recipient,
+    sender,
+    noteHash,
+    humanDeposit,
+    humanStartTime,
+    humanStopTime,
+    zkAsset,
+  };
+};
+
+function NewStreamTable({
   role,
   streamContract,
 }: {
-  role: string;
+  role: 'sender' | 'recipient';
   streamContract: Contract;
-}): ReactElement => {
+}): ReactElement {
   const userAddress = useAddress();
+  const aztec = useAztec();
   const { loading, error, data } = useQuery(
     role === 'sender' ? GET_SENDER_STREAMS : GET_RECIPIENT_STREAMS,
     {
       variables: { address: userAddress || '' },
       fetchPolicy: 'network-only',
     },
+  );
+
+  const cancelSelectedStream = useCallback(
+    (id) => cancelStream(aztec, streamContract, id, userAddress),
+    [aztec, streamContract, userAddress],
   );
 
   if (loading || error) {
@@ -53,52 +109,146 @@ const StreamTable = ({
     );
   }
 
+  const columns = generateColumns();
+
   const streamInProgress = data.streams.filter(
     (stream: Stream) => stream.cancellation == null,
   );
-  if (streamInProgress.length === 0) {
-    return (
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        spacing={3}
-      >
-        <Typography color="textSecondary">No streams to display</Typography>
-      </Grid>
+  console.log(streamInProgress);
+
+  const tableContents: TableRowData[] = streamInProgress.map(
+    (stream: Stream) => ({
+      ...humanReadableStream(stream),
+      humanStartTimeOrder: stream.startTime,
+      humanStopTimeOrder: stream.stopTime,
+      humanLastWithdrawTimeOrder: stream.lastWithdrawTime,
+    }),
+  );
+  return (
+    <Table
+      columns={columns}
+      data={tableContents}
+      defaultFixed
+      defaultOrder="desc"
+      defaultOrderBy={STREAM_TABLE_ID}
+      defaultRowsPerPage={25}
+      label="Transactions"
+      size={tableContents.length}
+      disableLoadingOnEmptyTable
+    >
+      {(sortedData: TableRowData[]): any =>
+        sortedData.map((row: TableRowData) => (
+          <NewStreamRow
+            key={row.id}
+            role={role}
+            row={row}
+            columns={columns}
+            cancelSelectedStream={cancelSelectedStream}
+          />
+        ))
+      }
+    </Table>
+  );
+}
+
+const NewStreamRow = ({
+  role,
+  row,
+  columns,
+  cancelSelectedStream,
+}: {
+  role: 'sender' | 'recipient';
+  row: TableRowData;
+  columns: Column[];
+  cancelSelectedStream: Function;
+}): ReactElement => {
+  const {
+    id,
+    humanStartTime,
+    humanStopTime,
+    noteHash,
+    recipient,
+    sender,
+    humanStartTimeOrder,
+    humanStopTimeOrder,
+    humanLastWithdrawTimeOrder,
+    zkAsset,
+  }: TableRowData = row;
+  const note: ZkNote | undefined = useDecodedNote(noteHash);
+  const displayName = useENSName(role === 'sender' ? recipient : sender);
+  const displayValue =
+    note?.value &&
+    convertToTokenValueDisplay(
+      note.value,
+      zkAsset.scalingFactor,
+      zkAsset.linkedToken.decimals,
     );
-  }
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>{role === 'recipient' ? 'Sender' : 'Recipient'}</TableCell>
-          <TableCell align="right">Value remaining</TableCell>
-          <TableCell align="right">Progress</TableCell>
-          <TableCell align="right">Start time</TableCell>
-          <TableCell align="right">End time</TableCell>
-          <TableCell align="right"></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {streamInProgress.map((stream: Stream) => (
-          <StreamRow
-            key={stream.id}
-            stream={stream}
-            streamContract={streamContract}
-            role={role}
-          />
-        ))}
-      </TableBody>
-    </Table>
+    <TableRow key={id} tabIndex={-1}>
+      <TableCell
+        align={columns[0].align}
+        component="td"
+        key={columns[0].id}
+        style={cellWidth(columns[0].width)}
+      >
+        {id}
+      </TableCell>
+      <TableCell
+        align={columns[1].align}
+        component="td"
+        key={columns[1].id}
+        style={cellWidth(columns[1].width)}
+      >
+        {displayName}
+      </TableCell>
+      <TableCell
+        align={columns[2].align}
+        component="td"
+        key={columns[2].id}
+        style={cellWidth(columns[2].width)}
+      >
+        {`${displayValue} ${zkAsset.symbol}`}
+      </TableCell>
+      <TableCell
+        align={columns[3].align}
+        component="td"
+        key={columns[3].id}
+        style={cellWidth(columns[3].width)}
+      >
+        <DoubleProgressBar
+          startTime={humanStartTimeOrder}
+          stopTime={humanStopTimeOrder}
+          lastWithdrawTime={humanLastWithdrawTimeOrder}
+        />
+      </TableCell>
+      <TableCell
+        align={columns[4].align}
+        component="td"
+        key={columns[4].id}
+        style={cellWidth(columns[4].width)}
+      >
+        {humanStartTime}
+      </TableCell>
+      <TableCell
+        align={columns[5].align}
+        component="td"
+        key={columns[5].id}
+        style={cellWidth(columns[5].width)}
+      >
+        {humanStopTime}
+      </TableCell>
+      <TableCell component="td">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={(): Promise<void> => cancelSelectedStream(row.id)}
+        >
+          Cancel
+        </Button>
+      </TableCell>
+    </TableRow>
   );
 };
 
-StreamTable.propTypes = {
-  streamContract: PropTypes.any.isRequired,
-  role: PropTypes.string.isRequired,
-};
-
-export default StreamTable;
+export default NewStreamTable;
---
+++ packages/react-app/src/components/Table/TableHead.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from 'react';
+
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import { Column } from './columns';
+
+export const cellWidth = (width?: number): object | undefined => {
+  if (!width) {
+    return undefined;
+  }
+
+  return {
+    maxWidth: `${width}px`,
+  };
+};
+
+function TableHeader(props: any): ReactElement {
+  const { columns, order, orderBy, onSort } = props;
+
+  const changeSort = (property: string, orderAttr: boolean) => () => {
+    onSort(property, orderAttr);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {columns.map((column: Column) => (
+          <TableCell
+            align={column.align}
+            key={column.id}
+            padding={column.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === column.id ? order : false}
+          >
+            {column.static ? (
+              <div style={column.style}>{column.label}</div>
+            ) : (
+              <TableSortLabel
+                active={orderBy === column.id}
+                direction={order}
+                onClick={changeSort(column.id, column.order)}
+                style={column.style}
+              >
+                {column.label}
+              </TableSortLabel>
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export default TableHeader;
