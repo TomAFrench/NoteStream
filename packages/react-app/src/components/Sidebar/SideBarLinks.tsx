f99a4b5f569f35ddd45b383639e427cbe633596d
Tom French <tom@tomfren.ch>
2020-07-25 12:23:16 +0100

    feat: remove Transak

--- packages/react-app/src/components/Sidebar/SideBarLinks.tsx
+++ packages/react-app/src/components/Sidebar/SideBarLinks.tsx
@@ -1,5 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react';
-import { useTheme } from '@material-ui/core/styles';
+import React, { ReactElement } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -9,16 +8,11 @@ import Divider from '@material-ui/core/Divider';
 import HomeIcon from '@material-ui/icons/Home';
 import LocalAtmIcon from '@material-ui/icons/LocalAtm';
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
 import HelpIcon from '@material-ui/icons/Help';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
-import TransakSDK from '@transak/transak-sdk';
 import Link from '../Link';
-
-import setupTransak from '../../utils/transak';
-import { useAddress } from '../../contexts/OnboardContext';
 
 const mainLinks = [
   { text: 'Home', icon: <HomeIcon />, url: '/' },
@@ -55,42 +49,26 @@ const NavLinkItem = ({
   </ListItem>
 );
 
-const SideBarLinks = (): ReactElement => {
-  const theme = useTheme();
-  const userAddress = useAddress();
-  const [transak, setTransak] = useState<typeof TransakSDK>();
-
-  useEffect(() => {
-    setTransak(setupTransak(userAddress, theme.palette.primary.main));
-  }, [userAddress, theme.palette.primary.main]);
-
-  return (
-    <List>
-      {mainLinks.map((link, index) => (
-        <NavLinkItem
-          url={link.url}
-          icon={link.icon}
-          text={link.text}
-          key={index}
-        />
-      ))}
-      <Divider />
-      <ListItem button disabled onClick={(): Promise<void> => transak.init()}>
-        <ListItemIcon>
-          <ShoppingCartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Buy crypto" />
-      </ListItem>
-      {secondaryLinks.map((link, index) => (
-        <NavLinkItem
-          url={link.url}
-          icon={link.icon}
-          text={link.text}
-          key={index}
-        />
-      ))}
-    </List>
-  );
-};
+const SideBarLinks = (): ReactElement => (
+  <List>
+    {mainLinks.map((link, index) => (
+      <NavLinkItem
+        url={link.url}
+        icon={link.icon}
+        text={link.text}
+        key={index}
+      />
+    ))}
+    <Divider />
+    {secondaryLinks.map((link, index) => (
+      <NavLinkItem
+        url={link.url}
+        icon={link.icon}
+        text={link.text}
+        key={index}
+      />
+    ))}
+  </List>
+);
 
 export default SideBarLinks;
